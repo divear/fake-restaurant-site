ac13a6b29fe2746c6635b411c84786d4e01ffa98
lukascobit <lukyn.odehnal333@gmail.com>
2023-08-12 09:57:46 +0200

    more boiler

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div>
-
-			<h1>U LABUŽNICKÉHO STOLU </h1>
-			<h2>Tradiční české restaurace.</h2>
-			<button>Obědová nabídka</button>
-			<button>Večerní nabídka</button>
+			<title>U Labužnického stolu</title>
+			<div className="intro">
+				<h1>U Labužnického stolu</h1>
+				<h2>Tradiční české restaurace.</h2>
+				<button>Obědová nabídka</button>
+				<button>Večerní nabídka</button>
+			</div>
 		</div>
 	);
 }
