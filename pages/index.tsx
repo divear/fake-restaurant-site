b2243588306f8f6ce47459ad0e1cbf206a4ed3e3
lukascobit <lukyn.odehnal333@gmail.com>
2023-08-14 22:39:45 +0200

    fix some css

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,12 @@ export default function Home() {
 		<div>
 			<title>U Labužnického stolu</title>
 			<div className="intro">
-				<h1>U Labužnického stolu</h1>
-				<h2>Tradiční české restaurace.</h2>
-				<button>Obědová nabídka</button>
-				<button>Večerní nabídka</button>
+				<h1 className="headerText">U Labužnického stolu</h1>
+				<h2 className="subText">Tradiční české restaurace.</h2>
+				<div className="subButtons">
+					<button>Obědová nabídka</button>
+					<button>Večerní nabídka</button>
+				</div>
 			</div>
 		</div>
 	);
