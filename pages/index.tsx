1714145257ecc6915d7bff88787e887dcc065394
lukascobit <lukyn.odehnal333@gmail.com>
2023-08-16 22:56:09 +0200

    tradic text

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,15 @@ export default function Home() {
 				<h1 className="headerText">U Labužnického stolu</h1>
 				<h2 className="subText">Tradiční česká restaurace.</h2>
 				<div className="subButtons">
-					<button>Obědová nabídka</button>
-					<button>Večerní nabídka</button>
+					<button onClick={() => open("/obedova-nabidka")}>Obědová nabídka</button>
+					<button onClick={() => open("/vecerni-nabidka")}>Večerní nabídka</button>
 				</div>
+			</div>
+			<div className="leftText">
+				<h1 className="leftHeader">Restaurace s rodinnou tradicí</h1>
+				<p>
+					Od svého založení v roce 1974 jsme nezměnili menu
+				</p>
 			</div>
 		</div>
 	);
---
+++ pages/obedova-nabidka.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Obedovanabidka() {
+    return (
+        <div>
+            <h1 className='headerText'>Obědová nabídka</h1>
+        </div>
+    )
+}
+
+export default Obedovanabidka
