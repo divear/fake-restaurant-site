3430f7b1d2851d8b4b8dceffbd52cf02938f2d90
lukascobit <lukyn.odehnal333@gmail.com>
2023-09-12 21:36:35 +0200

    add image src

--- pages/o-restauraci.tsx
+++ pages/o-restauraci.tsx
@@ -6,8 +6,11 @@ function ORestauraci() {
             <title>O Restauraci</title>
             <h1 className='pageHeader'>O Restauraci</h1>
 
-            Vítejte na naší restauraci s bohatou historií! Jsme hrdí majitelé této úžasné restaurace, kterou jsme založili již v roce 1974. Naše restaurace se v průběhu desetiletí stala neodmyslitelnou součástí české kulturní krajiny a místem, kde můžete ochutnat pravou a autentickou českou kuchyni.
-            <img src="" alt="" />
+            <h2 className='floatLeft'>
+                Vítejte na naší restauraci s bohatou historií! Jsme hrdí majitelé této úžasné restaurace, kterou jsme založili již v roce 1974. Naše restaurace se v průběhu desetiletí stala neodmyslitelnou součástí české kulturní krajiny a místem, kde můžete ochutnat pravou a autentickou českou kuchyni.
+
+            </h2>
+            <img className='floatRight' src="https://nomadparadise.com/wp-content/uploads/2019/11/05czechfoods.jpg" alt="" />
 
             Co dělá naší restauraci tak jedinečnou? Jedním z našich unikátních prvků je fakt, že jsme nikdy nezměnili naše menu. Držíme se tradic, které jsou pevně zakořeněny v naší kuchyni a předáváme je z generace na generaci. Věříme, že zachování původního menu je způsob, jak uctít naše předky a nabídnout našim hostům pravou chuť minulosti.
             Naše restaurace je nejenom o jídle, ale také o cestě časem. Když navštívíte naše prostory, přenesete se do doby, kdy se jednoduché suroviny a láskyplná péče proměňovaly v lahodné pokrmy, které jsou dnes stále oblíbené. Naše menu zahrnuje klasická česká jídla, která jsou známá po celém Česku. Každý kousek jídla, který opouští naši kuchyni, je plný historie a oddanosti.
