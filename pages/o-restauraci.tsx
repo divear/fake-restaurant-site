1d90e0e2a6c29182c4b5b8782ed3381a9b42fee3
lukascobit <lukyn.odehnal333@gmail.com>
2023-09-13 21:00:11 +0200

    orestauraci page

--- pages/o-restauraci.tsx
+++ pages/o-restauraci.tsx
@@ -6,16 +6,27 @@ function ORestauraci() {
             <title>O Restauraci</title>
             <h1 className='pageHeader'>O Restauraci</h1>
 
-            <h2 className='floatLeft'>
+            <img className='orestauraciImg' src="https://nomadparadise.com/wp-content/uploads/2019/11/05czechfoods.jpg" alt="" />
+            <h2 className=''>
                 Vítejte na naší restauraci s bohatou historií! Jsme hrdí majitelé této úžasné restaurace, kterou jsme založili již v roce 1974. Naše restaurace se v průběhu desetiletí stala neodmyslitelnou součástí české kulturní krajiny a místem, kde můžete ochutnat pravou a autentickou českou kuchyni.
 
             </h2>
-            <img className='floatRight' src="https://nomadparadise.com/wp-content/uploads/2019/11/05czechfoods.jpg" alt="" />
 
-            Co dělá naší restauraci tak jedinečnou? Jedním z našich unikátních prvků je fakt, že jsme nikdy nezměnili naše menu. Držíme se tradic, které jsou pevně zakořeněny v naší kuchyni a předáváme je z generace na generaci. Věříme, že zachování původního menu je způsob, jak uctít naše předky a nabídnout našim hostům pravou chuť minulosti.
-            Naše restaurace je nejenom o jídle, ale také o cestě časem. Když navštívíte naše prostory, přenesete se do doby, kdy se jednoduché suroviny a láskyplná péče proměňovaly v lahodné pokrmy, které jsou dnes stále oblíbené. Naše menu zahrnuje klasická česká jídla, která jsou známá po celém Česku. Každý kousek jídla, který opouští naši kuchyni, je plný historie a oddanosti.
-            Jsme tu pro vás, abychom vás provedli nezapomenutelným gastronomickým zážitkem. Naše restaurace není jen místem ke stravování, je to místo, kde můžete prozkoumat a ocenit krásu a bohatství české kuchyně. Věříme, že naše oddanost tradici a autenticitě se odráží v každém detailu našeho podniku.
-            Děkujeme, že jste si vybrali naši restauraci, abyste nám dovolili vás hostit a poskytnout vám vzácnou příležitost ochutnat nezapomenutelné chutě českého kulturního dědictví. Těšíme se na vaši návštěvu!
+            <p className='orestauraciText'>
+
+                Co dělá naší restauraci tak jedinečnou? Jedním z našich unikátních prvků je fakt, že jsme nikdy nezměnili naše menu. Držíme se tradic, které jsou pevně zakořeněny v naší kuchyni a předáváme je z generace na generaci. Věříme, že zachování původního menu je způsob, jak uctít naše předky a nabídnout našim hostům pravou chuť minulosti.
+                Naše restaurace je nejenom o jídle, ale také o cestě časem. Když navštívíte naše prostory, přenesete se do doby, kdy se jednoduché suroviny a láskyplná péče proměňovaly v lahodné pokrmy, které jsou dnes stále oblíbené. Naše menu zahrnuje klasická česká jídla, která jsou známá po celém Česku. Každý kousek jídla, který opouští naši kuchyni, je plný historie a oddanosti.
+                Jsme tu pro vás, abychom vás provedli nezapomenutelným gastronomickým zážitkem. Naše restaurace není jen místem ke stravování, je to místo, kde můžete prozkoumat a ocenit krásu a bohatství české kuchyně. Věříme, že naše oddanost tradici a autenticitě se odráží v každém detailu našeho podniku.
+                Děkujeme, že jste si vybrali naši restauraci, abyste nám dovolili vás hostit a poskytnout vám vzácnou příležitost ochutnat nezapomenutelné chutě českého kulturního dědictví. Těšíme se na vaši návštěvu!
+            </p>
+            <div className="galerie">
+
+                <p className='pageHeader'>Galerie: </p>
+                <img src="http://hrcava.wz.cz/foto/u_sikory_2018.jpg" alt="" />
+                <img src="http://hrcava.wz.cz/foto/usikory2.jpg" alt="" />
+                <img src="http://hrcava.wz.cz/foto/usikory1.jpg" alt="" />
+                <img src="http://hrcava.wz.cz/foto/minibar1.jpg" alt="" />
+            </div>
         </div >
     )
 }
